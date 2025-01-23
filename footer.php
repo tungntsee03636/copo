4be244974382b85e9817c2a60b392d9f1d2dc393
グェン タン トゥン <n1tung@sg.sougo-staff.co.jp>
2025-01-23 10:17:05 +0700

    a

--- footer.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-?>
-<!--            FOOTER-->
-     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
-<?php wp_footer(); ?>
-</body>
-</html>
