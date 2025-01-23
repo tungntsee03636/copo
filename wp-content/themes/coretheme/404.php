20aae03a5c757257dddd332b8fdc88218cd0564c
グェン タン トゥン <n1tung@sg.sougo-staff.co.jp>
2025-01-23 09:42:51 +0700

    update core wordpress

---
+++ wp-content/themes/coretheme/404.php
@@ -0,0 +1,10 @@
+
+<?php 
+ get_header();
+?>
+<a href="<?php echo get_home_url(); ?>">
+    <img style="width:100%" src="https://blog.minnano-tokugi.com/wp-content/uploads/2023/06/opps.png">
+</a>
+
+
+<?php get_footer();?>
---
+++ wp-content/themes/coretheme/footer.php
@@ -0,0 +1,9 @@
+
+<?php
+?>
+<!--            FOOTER-->
+     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
