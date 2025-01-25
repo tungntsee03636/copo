7a8fb49c60e208deb1c2c143648113360c7d1479
グェン タン トゥン <n1tung@sg.sougo-staff.co.jp>
2025-01-25 16:23:46 +0700

    update

--- wp-content/themes/Copo/footer.php
+++ wp-content/themes/Copo/footer.php
@@ -16,14 +16,14 @@
     </div>
 </div>
 <!--            FOOTER-->
-<footer>
+<div class="footer">
     <picture>
         <source media="(max-width: 767px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/footer-sp.png 2x">
         <source media="(min-width: 768px)" srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/footer.png 2x">
         <img class="sizes" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/footer.png" alt="">
     </picture>
-</footer>
-     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+</div>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
 </div>
 </body>
 </html>
--- wp-content/themes/Copo/header.php
+++ wp-content/themes/Copo/header.php
@@ -4,24 +4,24 @@
     <meta charset="UTF-8"/>
      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,minimum-scale=0.5">
+
     <meta name="format-detection" content="telephone=no">
     <title><?php echo get_the_title(); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/main.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 
-    
     <?php
     wp_head();
     ?>
 </head>
 
 
-<body <?php body_class(); ?>>
+<body>
 <div class="main-wrap">
 
 <!--HEADER-->
-<header class="main-header">
+<div class="main-header">
     <div class="inner w-bg fl">
         <div class="logo">
             <a href="<?php echo home_url(); ?>">
@@ -42,5 +42,6 @@
 
         <img class="ham sp-br" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ham.png">
     </div>
-</header>
+</div>
+
 
