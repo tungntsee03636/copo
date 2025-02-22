a5e78b96dd2962ae4dd27809a30d73d1604dc201
グェン タン トゥン <n1tung@sg.sougo-staff.co.jp>
2025-02-22 10:26:50 +0700

    update

--- wp-content/themes/Copo/archive-short-movie.php
+++ wp-content/themes/Copo/archive-short-movie.php
@@ -38,12 +38,30 @@ $movies = new WP_Query($args);
                         ?>
 
                         <div class="short-movie-item" data-id="<?php echo esc_attr($movie_id); ?>" style="cursor:pointer;">
-                            <div class="tiktok-video-wrapper">
-                                <blockquote class="tiktok-embed" cite="<?php echo esc_url($short_link); ?>" data-video-id="<?php echo esc_attr(basename($short_link)); ?>">
-                                    <a href="<?php echo esc_url($short_link); ?>" style="width: 100%"></a>
-                                </blockquote>
-                                <script async src="https://www.tiktok.com/embed.js"></script>
-                            </div>
+                            <?php
+                                if ($short_link) :
+                                    if (strpos($short_link, 'tiktok.com') !== false) :
+                                        // Nếu là link TikTok
+                                        ?>
+                                        <div class="video-wrapper">
+                                            <blockquote class="tiktok-embed" cite="<?php echo esc_url($short_link); ?>" data-video-id="<?php echo esc_attr(basename($short_link)); ?>" style="width: 100%;">
+                                                <a href="<?php echo esc_url($short_link); ?>"></a>
+                                            </blockquote>
+                                            <script async src="https://www.tiktok.com/embed.js"></script>
+                                        </div>
+                                    <?php
+                                    elseif (strpos($short_link, 'youtube.com') !== false || strpos($short_link, 'youtu.be') !== false) :
+                                        // Nếu là link YouTube
+                                        parse_str(parse_url($short_link, PHP_URL_QUERY), $yt_params);
+                                        $video_id = isset($yt_params['v']) ? $yt_params['v'] : basename(parse_url($short_link, PHP_URL_PATH));
+                                        ?>
+                                        <div class="video-wrapper">
+                                            <iframe class="youtube" width="100%" height="576px" src="https://www.youtube.com/embed/<?php echo esc_attr($video_id); ?>" frameborder="0" allowfullscreen></iframe>
+                                        </div>
+                                    <?php
+                                    endif;
+                                endif;
+                            ?>
 
                         </div>
 
